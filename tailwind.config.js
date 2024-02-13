1b754eba3811a0887bdeb13904c62b88e4f5c7e3
shashankbhosagi <shashankbhosagi0121@gmail.com>
2024-02-13 20:54:22 +0530

    ✨ Add structure for search, result and write media query

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,19 @@ module.exports = {
       width: {
         1186: "1186px",
         1080: "1080px",
+        custom: "calc(100% - 24px)",
       },
       animation: {
         "float-space": "float-space 18s infinite",
+        "border-animation": "border-animation 1s infinite",
       },
       keyframes: {
         "float-space": {
+          "0%": { transform: "translateY(30px)" },
+          "50%": { transform: "translateY(80px)" },
+          "100%": { transform: "translateY(30px)" },
+        },
+        "border-animation": {
           "0%": { transform: "translateY(30px)" },
           "50%": { transform: "translateY(80px)" },
           "100%": { transform: "translateY(30px)" },
